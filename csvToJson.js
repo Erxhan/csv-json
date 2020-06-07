b1bde35abd61b433eb3602c2f685b15b2ff24d07
Erxhan Selmani <erxhanselmani@icloud.com>
2020-06-07 12:02:49 +0200

    add some comments

--- csvToJson.js
+++ csvToJson.js
@@ -5,8 +5,11 @@ fs.readFile('translations.tsv', (err, data) => {
         console.log('err ===>', err)
         return
     }
+
+    // The data from readFile is a big chunk Buffer => We transform it in String
     const bigString = data.toString()
 
+    // We split this big string by the return character and make rows
     const rows = bigString.split('\n')
 
     // ROW => [Variable, en, fr_BE, nl_BE]
@@ -27,12 +30,18 @@ fs.readFile('translations.tsv', (err, data) => {
         })
     })
 
+    // Make a new object where the keys are renamed properly (1 => 'en', 2 => 'fr_BE', 3 => 'nl_BE', ...)
     const newObj = {}
+
+    // Take only the first row, the headers and put them in an array
     const headers = rows[0].split('\t')
+
+    // Iterate over the object with index based names
     Object.entries(obj).forEach(([key, value]) => {
         newObj[headers[key]] = value
     })
 
+    // Iterate over each object in the new object and write a file based on the key ('en' => 'en.json', 'fr_BE' => 'fr_BE.json', 'nl_BE' => 'nl_BE.json')
     Object.entries(newObj).forEach(([key, value]) => {
         return fs.writeFile(`${key}.json`, JSON.stringify(value, null, 2), (err) => {
             if (err) throw err;
