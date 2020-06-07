09d683cb48d3822447bcabdc2248d367a7cb059e
Erxhan Selmani <erxhanselmani@icloud.com>
2020-06-07 11:46:37 +0200

    Object creation

--- csvToJson.js
+++ csvToJson.js
@@ -6,16 +6,33 @@ fs.readFile('translations.tsv', (err, data) => {
         return
     }
     const bigString = data.toString()
-    const rows = bigString.split('\t')
-    console.log('rows ===>', rows)
+
+    const rows = bigString.split('\n')
+
     // ROW => [Variable, en, fr_BE, nl_BE]
-    const header = rows[0]
-    const headerTitles = header.split(',')
-    // console.log('headerTitles ===>', headerTitles)
-    const exampleRow = rows[1]
-    // console.log('exampleRow ===>', exampleRow)
-    // ROW => ["Something", "something", "quelque chose", "iets"]
-    const split = bigString.split(',')
-    // console.log('split ===>', split)
-    // console.log('data ===>', data.toString())
+    let obj = {}
+    rows.forEach((row, rowIndex) => {
+        let rowSplit = row.split('\t')
+
+        rowSplit.forEach((rowCol, index) => {
+            if (rowIndex === 0) {
+                if (index !== 0) {
+                    obj[index] = {}
+                }
+            } else {
+                if (index !== 0) {
+                    obj[index] = {...obj[index], [rowSplit[0]]: rowSplit[index]}
+                }
+            }
+        })
+    })
+
+    const newObj = {}
+    const headers = rows[0].split('\t')
+    Object.entries(obj).forEach(([key, value]) => {
+        console.log('headers[key] ===>', headers[key])
+        newObj[headers[key]] = value
+    })
+
+    console.log('newObj ===>', newObj)
 })
